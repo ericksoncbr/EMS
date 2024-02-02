99d9e549006e25a413573f5704f060becf3350c8
ericksoncbr <157963955+ericksoncbr@users.noreply.github.com>
2024-02-03 07:12:18 +0800

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication16
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=App_Data\Database1.mdf;Integrated Security=True");
+            con.Open();
+
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO Registration(Fname,Email,Password) VALUES (@Fname,@Email,@Password)", con);
+            cmd.Parameters.AddWithValue("@Fname", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@Email", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@Password", TextBox3.Text);
+
+            cmd.ExecuteNonQuery();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Succesfuly Registered');", true);
+            con.Close();
+        }
+    }
+}
